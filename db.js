ebfd93acbf896f50c2bef3a0b93d57ddfde61338
Amauri Rodrigues <ardsj@yahoo.com.br>
2016-06-30 19:55:37 -0300

    Update db.js

--- db.js
+++ db.js
@@ -3,11 +3,11 @@ var fakeObj = require('./fake_objects.js')
 module.exports = function() { 
 
 	this.routes = {
-		"v1/products":fakeObj.generateFakeVinyls(),
-		"v1/users":[{"email": "teste", "firstName": "teste", "lastName": "teste", "password": "teste", "phone": 64646767764646,"username":"teste","id": 1
+		"products":fakeObj.generateFakeVinyls(),
+		"users":[{"email": "teste", "firstName": "teste", "lastName": "teste", "password": "teste", "phone": 64646767764646,"username":"teste","id": 1
 		  }],
-		"v1/users/login":[],
-		"v1/payment":[]
+		"users/login":[],
+		"payment":[]
 	}
 
 	return this;
